d00683a91504d875c762d0333a7b4251c1d4f122
Vic Spencer <victonio.b.spencer@gmail.com>
2019-03-08 15:44:16 -0600

    Update R Clustering utils.R

--- R Clustering utils.R
+++ R Clustering utils.R
@@ -2,7 +2,8 @@
 distance <- sqrt( (player1$x - player2$x)^2 + (player1$y - player2$y)^2 )
 
 #########
-# Clustering on numeric variables
+# Hierarchical Clustering on numeric variables
+
 # Calculate the distance between data points
 dist <- dist(two_players)
 
@@ -10,11 +11,139 @@ dist <- dist(two_players)
 scaled_data <- scale(data)
 dist_scaled <- dist(scaled_data)
 
+# Perform the hierarchical clustering using the complete linkage
+hc_players <- hclust(dist_players)
+
+# Calculate the assignment vector with a k of 2
+clusters_k2 <- cutree(hc_players,2)
+
+# Create a new dataframe storing these results
+lineup_k2_complete <- mutate(lineup, cluster = clusters_k2)
+
+# Count the cluster assignments
+count(lineup_k2_complete, cluster)
+
+# Plot the positions of the players and color them using their cluster
+ggplot(lineup_k2_complete, aes(x = x, y = y, color = factor(cluster))) +
+  geom_point()
+
+# Prepare the Distance Matrix
+dist_players <- dist(lineup)
+
+# Generate hclust for complete, single & average linkage methods
+hc_complete <- hclust(dist_players,"complete")
+hc_single <- hclust(dist_players,"single")
+hc_average <- hclust(dist_players,"average")
+
+# Plot & Label the 3 Dendrograms Side-by-Side
+# Hint: To see these Side-by-Side run the 4 lines together as one command
+par(mfrow = c(1,3))
+plot(hc_complete, main = 'Complete Linkage')
+plot(hc_single, main = 'Single Linkage')
+plot(hc_average, main = 'Average Linkage')
+
+library(dendextend)
+dist_players <- dist(lineup, method = 'euclidean')
+hc_players <- hclust(dist_players, method = "complete")
+
+# Create a dendrogram object from the hclust variable
+dend_players <- as.dendrogram(hc_players)
+
+# Plot the dendrogram
+plot(dend_players)
+
+# Color branches by cluster formed from the cut at a height of 20 & plot
+dend_20 <- color_branches(dend_players, h = 20)
+
+# Plot the dendrogram with clusters colored below height 20
+plot(dend_20)
+
+# Calculate Euclidean distance between customers
+dist_customers <- dist(customers_spend)
+
+# Generate a complete linkage analysis 
+hc_customers <- hclust(dist_customers)
+
+# Plot the dendrogram
+plot(hc_customers)
+
+# Create a cluster assignment vector at h = 15000
+clust_customers <- cutree(hc_customers,h = 15000)
+
+# Generate the segmented customers dataframe
+segment_customers <- mutate(customers_spend, cluster = clust_customers)
+
+dist_customers <- dist(customers_spend)
+hc_customers <- hclust(dist_customers)
+clust_customers <- cutree(hc_customers, h = 15000)
+segment_customers <- mutate(customers_spend, cluster = clust_customers)
+
+# Count the number of customers that fall into each cluster
+count(segment_customers,cluster)
+
+# Color the dendrogram based on the height cutoff
+dend_customers <- as.dendrogram(hc_customers)
+dend_colored <- color_branches(dend_customers, h= 15000)
+
+# Plot the colored dendrogram
+plot(dend_colored)
+
+# Calculate the mean for each category
+segment_customers %>% 
+  group_by(cluster) %>% 
+  summarise_all(funs(mean(.)))
 
 ###########
-# Clustering on categorical variables
+# Hierarchical Clustering on categorical variables
 # Dummify data
 dummy_data <- dummy.data.frame(data)
 
-# Calculate the Distance
-dist_survey <- dist(dummy_survey,method = 'binary')
+###############################################################################################################
+
+# K-Means Clustering
+
+# Build a kmeans model
+model_km2 <- kmeans(lineup, centers = 2)
+
+# Extract the cluster assignment vector from the kmeans model
+clust_km2 <- model_km2$cluster
+# Create a new dataframe appending the cluster assignment
+lineup_km2 <- mutate(lineup, cluster = clust_km2)
+
+# Plot the positions of the players and color them using their cluster
+ggplot(lineup_km2, aes(x = x, y = y, color = factor(cluster))) +
+  geom_point()
+
+## Determine the proper value for k (number of clusters)
+# Use map_dbl to run many models with varying value of k (centers)
+tot_withinss <- map_dbl(1:10,  function(k){
+  model <- kmeans(x = lineup, centers = k)
+  model$tot.withinss
+})
+
+# Generate a data frame containing both k and tot_withinss
+elbow_df <- data.frame(
+  k = 1:10,
+  tot_withinss = tot_withinss
+)
+
+# Plot the elbow plot
+ggplot(elbow_df, aes(x = k, y = tot_withinss)) +
+  geom_line() +
+  scale_x_continuous(breaks = 1:10)
+
+### Perform silhouette analysis to evaluate how well clustered different k values are
+library(cluster)
+
+# Generate a k-means model using the pam() function with a k = 2
+pam_k2 <- pam(lineup, k = 2)
+
+# Plot the silhouette visual for the pam_k2 model
+plot(silhouette(pam_k2))
+
+# Generate a k-means model using the pam() function with a k = 3
+pam_k3 <- pam(lineup,k =3)
+
+# Plot the silhouette visual for the pam_k3 model
+plot(silhouette(pam_k3))
+
